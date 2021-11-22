9733b04707ba62c2cf2a2f8024f54707e8455bd4
web-dave <dave@webdave.de>
2021-11-22 13:19:00 +0100

    my own state

--- src/app/book/book-api.service.ts
+++ src/app/book/book-api.service.ts
@@ -1,31 +1,35 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 import { Book } from './models';
 
 @Injectable({ providedIn: 'root' })
-export class BookApiService {
+export class BookApiService implements OnDestroy {
+  kill = false;
   private endpoint = 'http://localhost:4730/books';
-
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(`${this.endpoint}`);
+    return this.http.get<Book[]>(`${this.endpoint}`).pipe(takeWhile(() => !this.kill));
   }
 
   getByIsbn(isbn: string): Observable<Book> {
-    return this.http.get<Book>(`${this.endpoint}/${isbn}`);
+    return this.http.get<Book>(`${this.endpoint}/${isbn}`).pipe(takeWhile(() => !this.kill));
   }
 
   create(book: Book): Observable<Book> {
-    return this.http.post<Book>(`${this.endpoint}`, book);
+    return this.http.post<Book>(`${this.endpoint}`, book).pipe(takeWhile(() => !this.kill));
   }
 
   update(isbn: string, patch: Partial<Book>): Observable<Book> {
-    return this.http.patch<Book>(`${this.endpoint}/${isbn}`, patch);
+    return this.http.patch<Book>(`${this.endpoint}/${isbn}`, patch).pipe(takeWhile(() => !this.kill));
   }
 
   delete(isbn: string): Observable<Book> {
-    return this.http.delete<Book>(`${this.endpoint}/${isbn}`);
+    return this.http.delete<Book>(`${this.endpoint}/${isbn}`).pipe(takeWhile(() => !this.kill));
+  }
+  ngOnDestroy() {
+    this.kill = true;
   }
 }
---
+++ src/app/book/state.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { Book } from './models';
+
+@Injectable({ providedIn: 'root' })
+export class StateService {
+  // data
+  private data: Book[] = [];
+  private data$$ = new BehaviorSubject<Book[]>(this.data);
+  //getData
+  // eslint-disable-next-line @typescript-eslint/member-ordering
+  public data$: Observable<Book[]> = this.data$$.asObservable();
+
+  getBookByIsbn(isbn: string): Observable<Book> {
+    return this.data$.pipe(
+      map(books => books.find(book => book.isbn === isbn)),
+      filter((b): b is Book => !!b)
+    );
+  }
+
+  //setAll
+  setAll(books: Book[]): void {
+    this.data = books;
+    this.data$$.next(this.data);
+  }
+
+  // Update
+  uppdateOne(update: Book) {
+    this.data = this.data.map(book => (book.isbn === update.isbn ? update : book));
+    this.data$$.next(this.data);
+  }
+
+  // add
+  addOne(book: Book): void {
+    this.data.push(book);
+    this.data$$.next(this.data);
+  }
+
+  // delete
+  deleteOne(isbn: string) {
+    this.data = this.data.filter(book => book.isbn !== isbn);
+    this.data$$.next(this.data);
+  }
+}
