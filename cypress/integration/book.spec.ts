d21ff4a6c9f8b6746f1c7ab92f14e073e346e15a
web-dave <dave@webdave.de>
2021-11-23 12:56:17 +0100

    mock request

--- cypress/integration/book.spec.ts
+++ cypress/integration/book.spec.ts
@@ -1,12 +1,25 @@
 describe('As a user I want to', () => {
   const randomISBN = String(Math.floor(1000000000000 + Math.random() * 900000));
   before(() => {
+    cy.intercept('http://localhost:4730/books').as('foo'); //.its('response.body').should('contain', 'Java Web Scraping Handbook');
     cy.visit('/');
+    cy.wait(['@foo']);
+  });
+  after(() => {
+    cy.request('DELETE', 'http://localhost:4730/books/' + randomISBN);
   });
   it('see the heading of my app.', () => {
     findTExtInElem('mat-toolbar', 'BOOK MONKEY');
   });
   it('check if a book can be created.', () => {
+    cy.intercept('POST', 'http://localhost:4730/books').as('postbook');
+    // cy.intercept('POST', 'http://localhost:4730/books', request => {
+    //   console.log(request);
+    //   request.continue(response => {
+    //     expect(response.statusCode).eq(201);
+    //   });
+    // });
+
     // go to new Book
     cy.get('[routerlink="books/new"]').click();
     // save btn disabled
@@ -40,6 +53,8 @@ describe('As a user I want to', () => {
     cy.get('.mat-raised-button').click();
     // find book in list
     cy.get(`[ng-reflect-router-link="${randomISBN}"]`).should('exist');
+
+    cy.wait('@postbook').its('response.statusCode').should('eq', 201);
   });
 });
 
